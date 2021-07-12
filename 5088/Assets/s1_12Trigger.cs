8a593034671f38ef51ae18a942699deef6713b70
YuhyunLee <0902helena@naver.com>
2021-07-12 13:25:51 +0900

    5088 Pitfall v1.9 업로드

---
+++ 5088/Assets/s1_12Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1_12Trigger : MonoBehaviour
+{
+    public bool s1_12 = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        s1_12 = true;
+        // Box Collider 끄고
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+}
